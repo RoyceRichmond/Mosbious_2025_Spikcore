4265bc9a5b534a8a18070860e33de7bda3525d40
RoyceRichmond <miles-militis@hotmail.com>
2025-08-15 04:08:07 +0200

    add the floor plan for the dry run, sw matrix and proposed core elements

---
+++ designs/klayout/copying.sh
@@ -0,0 +1,4 @@
+cp layout_AH_neuron/AH_neuron.gds ~/.klayout/libraries/
+cp layout_LIF_ring/LIF.gds ~/.klayout/libraries/
+cp layout_synapse/synapse.gds ~/.klayout/libraries/
+cp layout_TG_bootstraped/TG_bootstrapped.gds ~/.klayout/libraries/
