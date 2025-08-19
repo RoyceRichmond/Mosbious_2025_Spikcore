fd8671a3674a05f298306ad7cbd787ef54f18693
RoyceRichmond <miles-militis@hotmail.com>
2025-08-19 07:16:51 +0200

    added the copying executable file to copy the gds from the project into klayout libraries

--- designs/klayout/copying.sh
+++ designs/klayout/copying.sh
@@ -1,4 +1,6 @@
-cp layout_AH_neuron/AH_neuron.gds ~/.klayout/libraries/
-cp layout_LIF_ring/LIF.gds ~/.klayout/libraries/
-cp layout_synapse/synapse.gds ~/.klayout/libraries/
-cp layout_TG_bootstraped/TG_bootstrapped.gds ~/.klayout/libraries/
+#!/bin/bash
+
+cp -f layout_AH_neuron/AH_neuron.gds ~/.klayout/libraries/
+cp -f layout_LIF_ring/LIF.gds ~/.klayout/libraries/
+cp -f layout_synapse/synapse.gds ~/.klayout/libraries/
+cp -f layout_TG_bootstraped/TG_bootstrapped.gds ~/.klayout/libraries/
