a42357a7a53ffda1b1f917728bf44e08be401e84
RoyceRichmond <miles-militis@hotmail.com>
2025-08-24 18:35:24 +0200

    resolved drc error on mim cap, change to option B metal4/5 modified dry run and copying script

--- designs/klayout/copying.sh
+++ designs/klayout/copying.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-mkdir ~/.klayout/libraries/
-cp -f layout_AH_neuron/AH_neuron.gds ~/.klayout/libraries/
-cp -f layout_LIF_ring/LIF.gds ~/.klayout/libraries/
-cp -f layout_synapse/synapse.gds ~/.klayout/libraries/
-cp -f layout_TG_bootstraped/TG_bootstrapped.gds ~/.klayout/libraries/
+
+# Target directory
+TARGET_DIR="$HOME/.klayout/libraries"
+
+# Create directory if it doesn't exist
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Directory $TARGET_DIR does not exist. Creating..."
+    mkdir -p "$TARGET_DIR"
+else
+    echo "Directory $TARGET_DIR already exists."
+fi
+
+# Copy files
+cp -f layout_AH_neuron/AH_neuron.gds "$TARGET_DIR/"
+cp -f layout_LIF_ring/LIF.gds "$TARGET_DIR/"
+cp -f layout_synapse/synapse.gds "$TARGET_DIR/"
+cp -f layout_TG_bootstraped/TG_bootstrapped.gds "$TARGET_DIR/"
+cp -f Dry_run_gds/M13_SpikCore.gds "$TARGET_DIR/"
+
+echo "Files copied successfully to $TARGET_DIR"
