21a40aabd7b941555e72ed072852a3b22fe2542f
RoyceRichmond <miles-militis@hotmail.com>
2025-09-18 02:22:23 +0200

    lvs passed from the synapses and ah neurons layout, same layout as the one in the padframe

---
+++ designs/klayout/layout_ah_sym/run_lvs.tcl
@@ -0,0 +1,19 @@
+# Tcl script to run LVS on ah_sym
+
+if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/share/pdk}
+if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
+
+set pdklib ${PDK_ROOT}/${PDK}
+set techlibs ${pdklib}/libs.tech
+set reflibs ${pdklib}/libs.ref
+
+#set setupfile ${techlibs}/netgen/gf180mcuD_setup.tcl
+set setupfile /foss/designs/Mosbious_2025_Spikcore/designs/klayout/gf180mcuD_setup.tcl
+set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
+
+set circuit1 [readnet spice ah_sym_layout.spice]
+set circuit2 [readnet spice $sclib]
+
+readnet spice ah_sym.spice $circuit2
+
+lvs "$circuit1 ah_sym" "$circuit2 ah_sym"         $setupfile ah_sym_comp.out
